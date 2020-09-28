3b81bb3e025cb92b94a070cbd3a66701e0a7317c
alter <kamillodzinski09@gmail.com>
2020-09-28 21:55:45 +0200

    Aplication connection

---
+++ src/main/java/com/responsywnie/springdataexample/CarRepo.java
@@ -0,0 +1,8 @@
+package com.responsywnie.springdataexample;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepo extends CrudRepository<> {
+}
